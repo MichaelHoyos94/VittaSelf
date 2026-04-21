79a8145fffcdaed8e97cb4136d6e142af4f9ef40
MichaelAlejandroAH <lgiraldo@igniweb.com>
2026-04-20 19:12:36 -0500

    Ajustes para el avance de proceso disciplinario

--- Modules/Sanctions/app/Services/DisciplinaryCaseService.php
+++ Modules/Sanctions/app/Services/DisciplinaryCaseService.php
@@ -2,6 +2,7 @@
 
 namespace Modules\Sanctions\Services;
 
+use Exception;
 use Modules\Sanctions\Repositories\CatCaseStatusRepository;
 use Modules\Sanctions\Repositories\DisciplinaryCaseRepository;
 
@@ -26,7 +27,15 @@ class DisciplinaryCaseService
     }
     public function progressCase($id)
     {
-        $statuses = $this->caseStatusRepository->getAll();
-        return $this->repository->progressCase($id);
+        $disciplinaryCase = $this->repository->getById($id);
+        $currentIndex = $disciplinaryCase->case_status_id;
+        $nextIndex = $currentIndex + 1;
+        $currentStatus = $this->caseStatusRepository->getById($currentIndex);
+        $nextStatus = $this->caseStatusRepository->getById($currentIndex + 1);
+
+        if (!$currentStatus) throw new Exception("Invalid current status.");
+        if (!$nextStatus) throw new Exception("Last status reached.");
+        $disciplinaryCase->case_status_id = $nextStatus->id;
+        return $this->repository->save($disciplinaryCase);
     }
 }
