d2b70ac6392481afdf329aa5c156951f265eb7fe
MichaelAlejandroAH <powerbi@igniweb.com>
2026-04-27 17:29:43 -0500

    vista resoluciones

--- Modules/Sanctions/resources/assets/js/Pages/Resolutions/Index.jsx
+++ Modules/Sanctions/resources/assets/js/Pages/Resolutions/Index.jsx
@@ -1,12 +1,24 @@
+import Table from "@/Components/Table";
 import MainLayout from "@/Layouts/MainLayout";
 import { usePage } from "@inertiajs/react";
 
 export default function Index() {
     const { resolutions } = usePage().props;
     console.log(resolutions);
-    return <>
-        <h1>INDEX RESOLUTIONS</h1>
-    </>;
+    const columns = [
+        
+    ]
+    return (
+        <div className="p-4 rounded bg-white shadow">
+            <div>
+                <h1>Resolutions history</h1>
+                <p>Resolutions history</p>
+            </div>
+            <Table 
+
+            />
+        </div>
+    );
 }
 
-Index.layout = (page) => <MainLayout children={page} />
+Index.layout = (page) => <MainLayout children={page} />;
