a4333e23012a9086b696f30a25cfcc7f9bd183a2
MichaelAlejandroAH <powerbi@igniweb.com>
2026-04-28 10:45:39 -0500

    Ajuste en los estados de casos disciplinarios y vista de resoluciones

--- Modules/Sanctions/resources/assets/js/Pages/Resolutions/Index.jsx
+++ Modules/Sanctions/resources/assets/js/Pages/Resolutions/Index.jsx
@@ -6,8 +6,13 @@ export default function Index() {
     const { resolutions } = usePage().props;
     console.log(resolutions);
     const columns = [
-        
-    ]
+        { header: "ID", accessor: "id" },
+        { header: "EUI INVOLVED", accessor: "disciplinary_case_id" },
+        { header: "ADMIN", accessor: "disciplinary_case_id" },
+        { header: "RESOLUTION TYPE", accessor: "resolution_type" },
+        { header: "RESOLUTION TEXT", accessor: "resolution"},
+        { header: "RESOLUTION DATE", accessor: "created_at"}
+    ];
     return (
         <div className="p-4 rounded bg-white shadow">
             <div>
@@ -15,7 +20,8 @@ export default function Index() {
                 <p>Resolutions history</p>
             </div>
             <Table 
-
+                columns={columns}
+                emptyText="No resolutions found."
             />
         </div>
     );
