1e688a94c446cdce0d4a975482765593497c0d76
MichaelAlejandroAH <lgiraldo@igniweb.com>
2026-05-02 23:07:18 -0500

    Excepcion para manejo de sanciones

--- Modules/Sanctions/app/Services/SanctionEnforcementService.php
+++ Modules/Sanctions/app/Services/SanctionEnforcementService.php
@@ -1,11 +1,12 @@
 <?php
 
 namespace Modules\Sanctions\Services;
+
 use Modules\Sanctions\Repositories\SanctionEnforcementRepository;
 
 class SanctionEnforcementService
 {
-    public function __construct(protected SanctionEnforcementRepository $repository){}
+    public function __construct(protected SanctionEnforcementRepository $repository) {}
     public function getAll()
     {
         return $this->repository->getAll();
