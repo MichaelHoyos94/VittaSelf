97a9e4965d9daa0a9fbbfa92aac5c9dffa9cf1a3
MichaelAlejandroAH <powerbi@igniweb.com>
2026-06-05 10:01:58 -0500

    Ajustes en entidades: Relacion usuario y centros de costos

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -26,6 +27,7 @@ class User extends Authenticatable
         'phone',
         'email',
         'password',
+        'cost_center_id',
     ];
 
     /**
@@ -50,5 +52,10 @@ class User extends Authenticatable
             'password' => 'hashed',
             'role' => \App\Enums\Role::class,
         ];
+    }
+
+    public function costCenter(): BelongsTo
+    {
+        return $this->belongsTo(CostCenter::class, 'cost_center_id');
     }
 }
--- database/migrations/2026_05_19_190000_create_cost_centers_table.php
+++ database/migrations/2026_05_19_190000_create_cost_centers_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->string('photo', 64)->default('cost_center.png');
             $table->timestamps();
             $table->softDeletes();
-            $table->foreignId('user_id')->constrained('users');
         });
     }
 
