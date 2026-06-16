236788867c40eedaf1c54bed45271f9f113a5867
MichaelAlejandroAH <powerbi@igniweb.com>
2026-06-16 16:57:11 -0500

    Modelo producto, migracion y entidad

---
+++ app/Enums/Category.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Enums;
+
+enum Category : string
+{
+    case SUPPLEMENTS = 'Supplements';
+    case PET_CARE = 'Pet care';
+    case FOOD_VEVERAGE = 'Powder';
+    case PHARMACY = 'Tablets';
+    case PERSONAL_CARE = 'Personal care';
+    case BEAUTY = 'Beauty';
+    case HEALTH_CARE = 'Health care';
+    case MERCHANDISING = 'Merchandising';
+}
---
+++ app/Models/Product.php
@@ -0,0 +1,36 @@
+<?php 
+
+namespace App\Models;
+
+use App\Enums\Category;
+use App\Enums\Presentation;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $table = 'products';
+
+    protected $fillable = [
+        'name',
+        'description',
+        'cover',
+        'price',
+        'points',
+        'slug',
+        'stock',
+        'presentation',
+        'category',
+    ];
+
+
+    protected function casts()
+    {
+        return [
+            'category' => Category::class,
+            'presentation' => Presentation::class,
+        ];
+    }
+}
