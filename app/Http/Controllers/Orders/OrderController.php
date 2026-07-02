f2e205143b111849d6ae0c63febd1aabd2f0271c
MichaelAlejandroAH <lgiraldo@igniweb.com>
2026-07-01 19:53:50 -0500

    Avances en la vista checkout

--- app/Http/Controllers/Orders/OrderController.php
+++ app/Http/Controllers/Orders/OrderController.php
@@ -3,16 +3,20 @@
 namespace App\Http\Controllers\Orders;
 
 use App\Http\Controllers\Controller;
+use App\Services\CartService;
 use App\Services\OrderService;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 
 class OrderController extends Controller
 {
-    public function __construct(private OrderService $service) {}
+    public function __construct(private OrderService $service, private CartService $cartService) {}
     public function checkout()
     {
-        return Inertia::render('Orders/Checkout');
+        $cart = $this->cartService->getByUserId(auth()->user()->id);
+        return Inertia::render('Orders/Checkout', compact('cart'))->with([
+            'cart' => $cart,
+        ]);
     }
     public function create()
     {
