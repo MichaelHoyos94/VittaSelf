6586261a778770c36380f05772d352ea52fe24cf
MichaelAlejandroAH <maaguirreh@uqvirtual.edu.co>
2026-09-08 10:37:39 -0500

    Fix: Se ajusta el seeder para crear solo el superadmin.

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -23,31 +23,9 @@ class UserSeeder extends Seeder
                 'phone' => '3218095138',
                 'address' => 'Cra 12 # 32 - 93',
                 'email_verified_at' => now(),
-                'password' => Hash::make('password'),
+                'password' => Hash::make('@v1t4s3lf'),
                 'remember_token' => Str::random(10),
-            ],
-            [
-                'name' => 'Alejandro',
-                'last_name' => 'Aguirre',
-                'document_number' => '10949447886',
-                'email' => 'sistemasaux@vittaself.com',
-                'phone' => '3012884210',
-                'address' => 'Cra 13 # 23 - 94',
-                'email_verified_at' => now(),
-                'password' => Hash::make('password'),
-                'remember_token' => Str::random(10),
-            ],
-            [
-                'name' => 'Carlos Andres',
-                'last_name' => 'Garcia',
-                'document_number' => '1094944787',
-                'email' => 'asesor-armenia@vittaself.com',
-                'phone' => '3212884211',
-                'address' => 'Cll 21 # 23 - 55',
-                'email_verified_at' => now(),
-                'password' => Hash::make('password'),
-                'remember_token' => Str::random(10),
-            ],
+            ]
         ];
         foreach ($users as $user) {
             $model = User::withTrashed()->firstOrNew(['email' => $user['email']]);
@@ -77,9 +55,5 @@ class UserSeeder extends Seeder
     {
         $user = User::where('email', 'sistemas@vittaself.com')->first();
         $user?->assignRole('super-admin');
-        $user = User::where('email', 'sistemasaux@vittaself.com')->first();
-        $user?->assignRole('administrator');
-        $user = User::where('email', 'asesor-armenia@vittaself.com')->first();
-        $user?->assignRole('commercial-agent');
     }
 }
